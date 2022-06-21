5a13e12305cb46c2aafb7d7e0789dd1e7511a821
Marknjo <marknjo7@gmail.com>
2022-06-21 11:05:33 +0300

    Initial commit

---
+++ pages/events/[...slug].tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const FilteredEventsPage: NextPage = () => {
+  return (
+    <div>
+      <h1>Search events page</h1>
+    </div>
+  );
+};
+
+export default FilteredEventsPage;
---
+++ pages/events/[eventId].tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const EventsDetailPage: NextPage = () => {
+  return (
+    <div>
+      <h1>View a single event</h1>
+    </div>
+  );
+};
+
+export default EventsDetailPage;
